80d4398e24b278a5373051292e01ba2c26f8f34a
japko-dev <lukasz.jablonski.dev@gmail.com>
2024-08-12 14:27:55 +0200

    feat(app): database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   'https://utfs.io/f/deaf40ae-814f-4085-9daf-98cb34bc50b3-1e.png',
   'https://utfs.io/f/caff6436-ddd8-4b41-b283-fa9e895a2aff-1g.png',
@@ -9,10 +11,16 @@ const mockImages = mockUrls.map((url, index) => ({
   id : index + 1, url
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts)
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt=""/>
