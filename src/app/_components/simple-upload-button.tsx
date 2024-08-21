99521706492a3723f517e47265b2aaa808e43b8e
japko-dev <lukasz.jablonski.dev@gmail.com>
2024-08-21 22:14:47 +0200

    add styling and loading for upload sonner

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -21,16 +21,16 @@ const useUploadThingInputProps = (...args: Input) => {
   };
 
   return {
-    inputProps: {
+    inputProps : {
       onChange,
-      multiple: ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
-      accept: "image/*",
+      multiple : ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
+      accept : "image/*",
     },
-    isUploading: $ut.isUploading,
+    isUploading : $ut.isUploading,
   };
 };
 
-function UploadSvg(){
+function UploadSvg() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
          className="size-6">
@@ -40,14 +40,29 @@ function UploadSvg(){
   )
 }
 
+function UploadingSpinnerSVG() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/>
+      <path
+        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+        className="spinner_ajPY"/>
+    </svg>
+  )
+}
+
 export function SimpleUploadButton() {
   const router = useRouter()
 
   const { inputProps } = useUploadThingInputProps("imageUploader", {
-    onUploadBegin(){
-      toast('Uploading...', {
-        duration: 1000000,
-        id: 'upload-begin'
+    onUploadBegin() {
+      toast(
+        <div className="flex gap-2 flex-row text-white items-center">
+          <UploadingSpinnerSVG/>
+          <span className="text-lg">Uploading...</span>
+        </div>, {
+        duration : 1000000,
+        id : 'upload-begin'
       })
     },
     onClientUploadComplete() {
