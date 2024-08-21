96e063f73e2804014d3872e518d7dbd216c408c9
japko-dev <lukasz.jablonski.dev@gmail.com>
2024-08-21 20:35:52 +0200

    fixed dialog full screen, mafe compoennt for image view

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,4 +1,10 @@
-export default  function PhotoModal({
-  params: {id : photoId}} : {params: {id: string}}) {
-  return <div>{photoId}</div>
+import FullPageImageView from "~/components/full-image-page";
+
+export default function PhotoPAge({
+                                     params: {id : photoId}} : {params: {id: string}}) {
+  const idAsNumber = Number(photoId)
+
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id")
+
+  return <FullPageImageView id={idAsNumber}/>
 }
