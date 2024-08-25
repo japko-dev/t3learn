7cf529e0bb7786798e2e78e9c96bd4ac0a43d31f
japko-dev <lukasz.jablonski.dev@gmail.com>
2024-08-25 21:21:07 +0200

    add server side logging

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,8 @@ import { auth } from "@clerk/nextjs/server";
 import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import analytics from "~/server/analytics";
+import analyticsServerClient from "~/server/analytics";
 
 export async function getMyImages() {
   const user = auth();
@@ -39,6 +41,14 @@ export async function deleteImage(id: number) {
   await db
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  })
 
   redirect("/");
 }
